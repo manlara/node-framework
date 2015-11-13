77ef860be444dfdd8f73fcfa53d37dba9d1417d0
Macario Lara <mac@Macarios-MacBook-Pro-2.local>
2015-11-13 13:42:17 -0600

    policies added

--- app.js
+++ app.js
@@ -156,20 +156,32 @@ orm.initialize(config, function(err, models) {
     //passport section
       
 
-      passport.use(new LocalStrategy(
-        function(username, password, done) {
-          User.findOne({ username: username }, function (err, user) {
-            if (err) { return done(err); }
-            if (!user) {
-              return done(null, false, { message: 'Incorrect username.' });
-            }
-            if (!user.validPassword(password)) {
-              return done(null, false, { message: 'Incorrect password.' });
-            }
-            return done(null, user);
-          });
-        }
-      ));
+      // passport.use(new LocalStrategy(
+      //   function(username, password, done) {
+      //     User.findOne({ username: username }, function (err, user) {
+      //       if (err) { return done(err); }
+      //       if (!user) {
+      //         return done(null, false, { message: 'Incorrect username.' });
+      //       }
+      //       if (!user.validPassword(password)) {
+      //         return done(null, false, { message: 'Incorrect password.' });
+      //       }
+      //       return done(null, user);
+      //     });
+      //   }
+      // ));
+
+
+      // passport.serializeUser(function(user, done) {
+      //   done(null, user.id);
+      // });
+
+      // passport.deserializeUser(function(id, done) {
+      //   User.findById(id, function(err, user) {
+      //     done(err, user);
+      //   });
+      // });
+      
     //end
 
 
